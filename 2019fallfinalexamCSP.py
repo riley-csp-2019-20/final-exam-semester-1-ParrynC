0f53a7768d4d01e9016f3796177ebdd07e905e7f
ParrynC <55498319+ParrynC@users.noreply.github.com>
2019-12-18 10:59:02 -0500

    Add files via upload

---
+++ 2019fallfinalexamCSP.py
@@ -0,0 +1,46 @@
+#2019-20 Fall Computer Science Principles Final Exam
+
+#Ms. Haubold
+
+
+#Name
+#
+#Date
+#
+
+
+#### INSTRUCTIONS ####
+#Create an etch a sketch turtle game
+#The turtle should move with the arrow keys (up, down, left and right), and draw
+#Space should clear the screen
+#o and p should make the pen size bigger and smaller
+#u should pick the pen up or put the pen down
+#All code must be commented
+#BONUS
+#Add a feature to change colors
+#
+
+#import required libraries
+
+
+#create turtle
+
+
+
+#movement functions
+
+
+
+#color/drawing functions
+
+
+
+#create screen
+
+#bind to keypresses
+
+
+#listen
+
+
+#mainloop
